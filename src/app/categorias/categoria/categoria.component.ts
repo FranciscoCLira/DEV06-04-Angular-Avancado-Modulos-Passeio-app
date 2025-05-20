b80ae83994fbc401004212ecb1d1708eb844a671
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-20 01:00:34 -0300

    58-Trabalhando com Formularios Reativos (Reactive Forms)
    PS C:\DEV06-Angular\projetos\passeio-app> ng g class categorias/categoria
    
    CREATE src/app/categorias/categoria.spec.ts (173 bytes)
    CREATE src/app/categorias/categoria.ts (29 bytes)
    PS C:\DEV06-Angular\projetos\passeio-app>
    
    Adicionar codigo em src\app\categorias\categoria\categoria.component.ts
    
    Adicionar codigo em src\app\categorias\categoria\categoria.component.html
       <form id="categoriaForm" [formGroup]="camposForm" (ngSubmit)="salvar()">
    
        name="nome"
        formControlName="nome"
    
        name="descricao"
        formControlName="descricao"
    
     Vamos subir a aplicação e testar.  ==> ng serve

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-categoria',
   standalone: false,
+
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
 
+  constructor(){
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required)
+    })
+  }
+
+  salvar(){
+    console.log('Valors digitados: ', this.camposForm.value)
+    console.log('Está valido?', this.camposForm.valid)
+  }
 }
+
