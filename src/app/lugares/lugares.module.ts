7c472a7aeee7e33e7f5ac911650ffdf2e9ab0b17
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-26 22:42:13 -0300

    70-Adicionando os campos do formulário de lugares
    1. Criar a classe Lugares
    
     PS C:\DEV06-Angular\projetos\passeio-app> ng g class lugares/lugar
     CREATE src/app/lugares/lugar.ts (25 bytes)
    
    2. Adicionar os campos da classe e alterar elementos
    
    3. Subir aplicação ==> ng serve
       PS C:\DEV06-Angular\projetos\passeio-app> ng serve
    
       Local:   http://localhost:4200/

--- src/app/lugares/lugares.module.ts
+++ src/app/lugares/lugares.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LugaresRoutingModule } from './lugares-routing.module';
 import { LugarComponent } from './lugar/lugar.component';
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { LugarComponent } from './lugar/lugar.component';
   ],
   imports: [
     CommonModule,
-    LugaresRoutingModule
+    LugaresRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class LugaresModule { }
