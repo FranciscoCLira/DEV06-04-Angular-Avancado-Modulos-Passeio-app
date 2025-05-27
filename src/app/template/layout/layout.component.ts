b58ccb562c249f90c87f6b7316ebdecc0ef4be41
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-27 20:06:18 -0300

    80-Capturando eventos de rotas para definir o titulo e subtitulo de cada página
    1. Nas rotas em src\app\template\template-routing.module.ts
       acrescentar o titulo e subtitulo em data:   ==> dados
    
    2. Criar novo arquivo  src\app\template\layout\layoutprops.ts
        interface LayoutProps  ==> com os campos titulo e subtitulo
    
    3. Em layout.component.ts  vamos capturar o data: do arquivo novo acima.
            props: LayoutProps = { titulo: '', subTitulo: ''}
    
    4. Passar para a pagina layout.component.html o titulo e subtitulo
    
       Substituir:
          <h1 class="text-3x1 font-bold">Título</h1>
          <p class="test-lg mt-2">Subtitulo</p>
       Por
          <h1 class="text-3x1 font-bold">{{ props.titulo }}</h1>
          <p class="test-lg mt-2">{{ props.subTitulo }}</p>
    
     5. Agora, em layout.component.ts, irá obter o titulo e subtitulo informado
          props: LayoutProps = { titulo: '', subTitulo: ''}
          informado:
            props: LayoutProps = { titulo: 'Teste', subTitulo: 'subteste'}
    
          Salvar e testar a aplicação
    
     6.  Para acessar a propriedade data: de cada rota, precisa injetar
         em layoutcomponent.ts.

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { LayoutProps } from './layoutprops';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
+
 
 @Component({
   selector: 'app-layout',
@@ -7,5 +11,32 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  props: LayoutProps = { titulo: '', subTitulo: '' };
+
+  constructor(
+    private router: Router,
+    private ActivatedRoute: ActivatedRoute
+  ){}
+
+
+  // quando mudar de rota vai ativar o evento e capiturar a rota ativada 
+  ngOnInit(): void {
+    this.router.events
+    .pipe(
+      filter( () => this.ActivatedRoute.firstChild !== null ),
+      map( () => this.obterPropriedadeLayout() )
+    ).subscribe((props: LayoutProps) => this.props = props)
+
+  }
+
+  obterPropriedadeLayout() : LayoutProps {
+    let rotaFilha = this.ActivatedRoute.firstChild;
+
+    while(rotaFilha?.firstChild){
+      rotaFilha = rotaFilha.firstChild;
+    }
+
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 
 }
--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -11,17 +11,21 @@ const routes: Routes = [
       {
         path: 'categorias',
         loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data : { titulo: 'Categorias', subTitulo: 'Relize o cadastro de novas categorias' }
       },
       {
         path: 'lugares',
         loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data : { titulo: 'Lugares', subTitulo: 'Relize o cadastro de novos lugares' }
       },
       {
         path: 'galeria',
         loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data : { titulo: 'Lista de lugares legais', subTitulo: 'Descubra os melhores lugares para explorar e se divertir' }
+
       }
     ]
   }
