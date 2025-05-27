eb425582a8aa94771912b7d18ef3555c4912ebd2
Francisco Casemiro Lira <franciscoclira@hotmail.com>
2025-05-27 00:12:28 -0300

    72-Lógica para enviar o cadastro de lugares para API
     Em src\app\lugares\lugar.service.ts

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
 import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
 
 @Component({
   selector: 'app-lugar',
@@ -15,7 +16,10 @@ export class LugarComponent implements OnInit {
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor(private categoriaService: CategoriaService) {
+  constructor(
+    private categoriaService: CategoriaService,
+    private service: LugarService
+  ) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -38,6 +42,16 @@ export class LugarComponent implements OnInit {
   }
 
   salvar() {
-    console.log("valores: ", this.camposForm.value);
+    // console.log("valores: ", this.camposForm.value);
+    this.service.salvar(this.camposForm.value)
+      .subscribe({
+          next: (lugar) => {
+            console.log("Lugar salvo com sucesso: ", lugar);
+            this.camposForm.reset(); 
+          },
+          error: (erro) => { 
+            console.error("Erro ao salvar lugar: ", erro)
+          }
+        }) 
   }
 }
